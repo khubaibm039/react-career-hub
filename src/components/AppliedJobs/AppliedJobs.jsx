a5b1b354e38ec1ae5406078983e7f86eb9c64179
khubaib004 <khabbab607342@gmail.com>
2023-10-01 23:01:21 +0600

    Implement job filter

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,29 +6,50 @@ import { BiDollarCircle } from "react-icons/bi";
 
 const AppliedJobs = () => {
     const jobs = useLoaderData();
+
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+
+    const handleJobsFilter = filter => {
+        if (filter === "all") {
+            setDisplayJobs(appliedJobs);
+        }
+        else if (filter === "remote") {
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+        else if (filter === "onsite") {
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === "Onsite");
+            setDisplayJobs(onsiteJobs);
+        }
+    }
+
     useEffect(() => {
         const storedJobIds = getStoredJobApplications();
         if (jobs.length > 0) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
             setAppliedJobs(jobsApplied)
+            setDisplayJobs(jobsApplied)
         }
     }, [])
 
     return (
         <div>
             <h2>I applied : {appliedJobs.length}</h2>
-            <details className="dropdown mb-32">
-                <summary className="m-1 btn">Filter By</summary>
-                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 ">
-                    <li><a>All</a></li>
-                    <li><a>Remote</a></li>
-                    <li><a>Onsite</a></li>
-                </ul>
-            </details>
-            <div>
+            <div className="mt-48 flex justify-end">
+                <details className="dropdown ">
+                    <summary className="m-1 btn">Filter By</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52  absolute">
+                        <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                        <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                        <li onClick={() => handleJobsFilter('onsite')}><a>Onsite</a></li>
+                    </ul>
+                </details>
+            </div>
+
+            <div className="mb-56">
                 {
-                    appliedJobs.map((job) => <div key={job.id}>
+                    displayJobs.map((job) => <div key={job.id}>
                         <div className="grid grid-cols-4 border my-8 p-8">
                             <div className="w-60 h-60 flex justify-center items-center bg-gray-100  rounded-lg">
                                 <img className="h-12" src={job.logo} alt="" />
